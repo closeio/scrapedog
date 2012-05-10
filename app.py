e0ebad458b0f3a5028149fdae4a85d5f1a549a0f
kromped <kromped@yahoo.com>
2012-05-09 17:00:50 -0700

    david's dummy

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 import os
+import logging
+import json
 
+import flask
 from flask import Flask
+from flask import Markup
+from flask import request
+
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def main():
+    url = request.args.get('url', '')
+    if url:
+        r = requests.get(url)
+        return 'abc' 
+    else:
+        return 'Hello World!'
+
+@app.route('/dummy')
+def dummy():
+    obj = {'url': 'http://www.google.com/',
+           'title': 'Google',
+           'http_headers': {'Content-Type': 'text/html',
+                            'Status': 200 
+                            },
+           'meta_tags': {'description': 'The homepage of the internet.'}
+    }
+    return json.dumps(obj)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
