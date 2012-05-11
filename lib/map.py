6bc867c9babb6e1572dc88163d813ca5a9e79110
kromped <kromped@yahoo.com>
2012-05-10 17:57:37 -0700

    google map lib to check if address is valid

---
+++ lib/map.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import urllib2
+
+class GoogleMap:
+    CACHE = {}
+    
+    def check_address(self, address):
+        url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % urllib2.quote(address)
+        
+        if GoogleMap.CACHE.has_key(url):
+            result = GoogleMap.CACHE[url]
+        else:
+            response = requests.get(url)
+            response = json.loads(response.text)
+            if response['status'] == 'ZERO_RESULTS':
+                result = False
+            else:
+                result = response['results'][0]['formatted_address']
+                
+            GoogleMap.CACHE[url] = result
+            
+        return result
+        
+        
+# example:
+# gm = GoogleMap()
+# print gm.check_address('1001 S Main St\nMilpitas, CA')
